306f3db7d459bbb5a5bb4793383cb4112d52b8b2
AlperAkinci <alperakinci1@gmail.com>
2016-03-02 17:28:19 +0200

    TableView , AddName Action, Saving CoreData Added

--- HitList/ViewController.swift
+++ HitList/ViewController.swift
@@ -7,19 +7,100 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController ,UITableViewDataSource {
+    //MARK: - Outlets
+    @IBOutlet weak var tableView: UITableView!
+    
+    //MARK: - Modal
+    // it used to be var names = [String]()
+    var people = [NSManagedObject]()
+    
+    
+    //MARK: - addName Action
+    @IBAction func addName(sender: AnyObject) {
+        
+        let alert = UIAlertController(title: "New Name", message: "", preferredStyle: .Alert)
+        
+        let saveAction = UIAlertAction(title: "Save", style: .Default) { (action : UIAlertAction) -> Void in
+            
+            let textField = alert.textFields?.first
+            self.saveName(textField!.text!)
+            
+            self.tableView.reloadData()
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel) { (action: UIAlertAction) -> Void in
+        }
+        
+        //Add textfield to an alert
+        alert.addTextFieldWithConfigurationHandler { (textfield : UITextField) -> Void in
+            textfield.placeholder = "Add a new name"
+        }
+        
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        
+        presentViewController(alert, animated: true, completion: nil)
+        
+        
+    }
+    
+    //MARK: - View Controller Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+        title = "\"The List\""
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //MARK: - UITableViewDataSource
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return people.count
     }
-
-
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as UITableViewCell
+        
+        let person = people[indexPath.row]
+        
+        cell.textLabel?.text = person.valueForKey("name") as? String
+        
+        return cell
+    }
+    //MARK: - saveName Function
+    
+    func saveName (name : String) {
+        
+    //1 - Access ManagedObjectContext which lives in AppDelegate, to access it you must get reference of AppDelegate first
+    let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+    
+    let managedContext = appDelegate.managedObjectContext
+        
+    //2 - Create a new managedObject and insert it into a managed object context . 
+    let entity  = NSEntityDescription.entityForName("Person", inManagedObjectContext: managedContext)
+    
+    let person = NSManagedObject(entity: entity!, insertIntoManagedObjectContext: managedContext)
+    
+    //3 - With an NSManagedObject , set the name attribute using key-value coding
+    person.setValue(name, forKey: "name")
+    
+    //4 - Commit the changes to person and save to disk by calling save on the managed object context
+    
+    do {
+        try managedContext.save()
+        people.append(person)//to show person when tableView reloads
+    }catch let error as NSError{
+        print("Could not save \(error),\(error.userInfo)")
+    }
+        
+    
+        
+    }
+    
+    
+    
 }
 
